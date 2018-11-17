fb68996b6e3895893ca13fb8f0e14092ce6bb9e8
NolanHughes <Nhughes987@gmail.com>
2018-11-17 14:56:25 -0600

    Update user controller to have role in strong params.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,7 +36,7 @@ class Api::V1::UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :role)
   end
   
   def authorize
